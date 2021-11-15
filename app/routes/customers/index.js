4c3f5814ea4aa96082a02b96de1d264be0bddba6
LazoJankovic <lazar.jankovic@codaxy.com>
2021-11-15 11:39:42 +0100

    added images

--- app/routes/customers/index.js
+++ app/routes/customers/index.js
@@ -1,6 +1,7 @@
 import Controller from './Controller';
-import { TextField, Button, Grid, Pagination, LookupField } from 'cx/widgets';
+import { TextField, Button, Grid, Pagination, LookupField, Window, TextArea, DateField } from 'cx/widgets';
 import { getSearchQueryPredicate } from 'cx/util';
+import { LabelsLeftLayout } from 'cx/ui';
 export default () => (
     <cx>
         <div class="overflow-hidden flex flex-col text-gray-600" controller={Controller}>
@@ -16,12 +17,58 @@ export default () => (
 
                 <div class="flex-grow" />
 
-                <Button text="New customer" mod="primary" />
+                <Button
+                    text="New customer"
+                    mod="primary"
+                    onClick={(e, { store }) => {
+                        store.set('$page.contact.visible', true);
+                    }}
+                />
+                <Window
+                    title="Contact"
+                    visible={{ bind: '$page.contact.visible', defaultValue: false }}
+                    center
+                    style={{ width: '500px' }}
+                    modal
+                    draggable
+                    closeOnEscape
+                >
+                    <div style={{ padding: '20px' }} layout={{ type: LabelsLeftLayout, mod: 'stretch' }}>
+                        <TextField
+                            label="Name"
+                            value-bind="$page.contact.name"
+                            style={{ width: '100%' }}
+                            tooltip="A Tooltip"
+                        />
+                        <TextField label="Email" value-bind="$page.contact.email" style={{ width: '100%' }} />
+                        <TextArea
+                            label="Message"
+                            value-bind="$page.contact.message"
+                            rows={10}
+                            style={{ width: '100%' }}
+                        />
+                        <DateField label="Date" value-bind="$page.contact.date" />
+                    </div>
+                    <div putInto="footer" style={{ float: 'right' }} ws>
+                        <Button mod="primary">Submit</Button>
+                        <Button
+                            // this will cause the Window to close
+                            dismiss
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                </Window>
 
                 <Button icon-expr="{$page.loading} ? 'loading' : 'refresh'" onClick="onLoad" mod="hollow">
                     Refresh
                 </Button>
             </div>
+            {/* <Button
+                
+            >
+                Open
+            </Button> */}
             <Grid
                 class="flex-grow"
                 buffered
@@ -65,6 +112,21 @@ export default () => (
                             rowSpan: 2,
                         },
                         field: 'img',
+                        items: (
+                            <cx>
+                                <img
+                                    class="h-14"
+                                    src-bind="$record.image"
+                                    tooltip={{
+                                        items: (
+                                            <cx>
+                                                <img class="h-128" src-bind="$record.image" />
+                                            </cx>
+                                        ),
+                                    }}
+                                />
+                            </cx>
+                        ),
                     },
                     {
                         align: 'center',
@@ -78,7 +140,7 @@ export default () => (
                         sortable: true,
                         align: 'center',
                         aggregate: 'distinct',
-                        caption: {tpl: '{$record.country}'}
+                        caption: { tpl: '{$record.country}' },
                     },
                     {
                         header2: 'Ulca',
@@ -95,20 +157,25 @@ export default () => (
                         field: 'phone',
                     },
                     {
-                      header1:{
-                        text: 'delete',
-                        rowSpan: 2,
-                      },
-                      items: <cx> 
-                        <Button
-                        mod='hollow'
-                          icon='trash'
-                          onClick='onRemove'
-                        />
-                      </cx>
-                    }
+                        header1: {
+                            text: 'Country',
+                            rowSpan: 2,
+                        },
+                        field: 'country',
+                    },
+                    {
+                        header1: {
+                            text: 'delete',
+                            rowSpan: 2,
+                        },
+
+                        items: (
+                            <cx>
+                                <Button mod="hollow" icon="trash" onClick="onRemove" />
+                            </cx>
+                        ),
+                    },
                 ]}
-                
             />
             <div class="border-t p-2 flex  ">
                 <Pagination page-bind="$page.page" pageCount-bind="$page.pageCount" />
